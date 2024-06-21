46d2918dbd836c428b87e3029b08f7a01ba25400
Q-Bukold <bukold@uni-hildesheim.de>
2024-06-21 19:06:59 +0000

    minor fix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 #from src.hys_portal_scraper import Portal_Scraper
 #from src.scrapers.initiative_scraper import Initiative_Scraper
 from src.scrapers.feedback_scraper import Feedback_Scraper
-#from src.scrapers.attachment_scraper import Attachment_Scraper
+from src.scrapers.attachment_scraper import Attachment_Scraper
 
 #from src.database.seedlist_handler import SeedList_Handler
 
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     logger = logging.getLogger("sqlalchemy.engine")
 
     con = database_connection()
-    Feedback_Scraper(connection=con, wait_time=30).scrape_all()
+    Feedback_Scraper(connection=con, wait_time=10, stage_id="7929317").scrape_feedback() # wait_time = time between requests
     
